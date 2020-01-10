02ca86ce6838ca574dcd764ab7c1b83f55f11e44
Joel Santana <p3t3ca5206@gmail.com>
2020-01-10 10:08:12 -0300

    razer's first login screen front end

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css?family=Acme|Montserrat:300,400,600,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css" />
+        <title>Razer</title>
+    </head>
+    <body>  
+        <div class="line-width"></div>
+
+        <div class="alignment">
+            <div class="margin">
+                <div class="border_chroma">
+                    <div class="content">
+                        <div class="logo"></div>
+                        <input type="text"  class="Email" placeholder="Email" autocomplete="off" />
+                        <input type="password"  class="password" placeholder="Password" autocomplete="off" />
+                        <button class="btn-login">Login</button>
+                        <div class="content_bottom">
+                            <h4 class="forgot">Forgot password ?</h4>
+                            <h4 class="register">Create account</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="line-width_buttom"></div>
+    </body>
+</html>
